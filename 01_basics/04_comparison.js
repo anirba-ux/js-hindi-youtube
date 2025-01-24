d35651be0618884f3e377753e6b139e1aacf77e8
anirba-ux <anirbanaddastudent@gmail.com>
2025-01-24 04:28:47 +0000

    Comparison conversion various datatype

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02"> 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//Strict chace
+console.log("2" === 2);
+
+//This type of conversion please avoid always
